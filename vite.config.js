385945392c0d4c6a073a76821635b0584d5358ab
hyeon-oo <115524826+hyeon-oo@users.noreply.github.com>
2025-06-03 17:11:39 +0900

    다시복구

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-// vite.config.js
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  base: '/Guestbook/', // 예: /guestbook-site/
-  plugins: [react()],
-});
