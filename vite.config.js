2d72fb86f9a11f9ac8e6e9e0910bb8fd643be48e
hyeon-oo <ggimgim244@gmail.com>
2025-06-04 00:08:32 +0900

    배포_v4

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/', // 예: /guestbook-site/
+  base: '/Guestbook/', // 예: /guestbook-site/
   plugins: [react()],
 });
